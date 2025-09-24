0a94b8d14845265459a06a2dd95f46ee0aaf1264
Dinil Hansara <dragonhansara@gmail.com>
2025-09-24 13:15:52 +0530

    initial commit

---
+++ about.php
@@ -0,0 +1,69 @@
+<head>
+	<link rel="shortcut icon" type="image/x-icon" href="assets/images/x-icon.png">
+    <link rel="stylesheet" type="text/css" href="landing_page/assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="landing_page/assets/css/font-awesome.css">
+
+    <link rel="stylesheet" href="landing_page/assets/css/landing_page.css">
+	<style>
+	@import url('https://fonts.googleapis.com/css2?family=Abhaya+Libre:wght@600&display=swap');
+	</style>
+</head>
+
+<body>
+	<?php include("includes/headerSimple.php");?>
+    
+    <!-- ***** Welcome Area Start ***** -->
+    <div class="welcome-area" id="welcome">
+        <div class="header-text">
+            <div class="container">
+                <div class="row">
+                    <div class="left-text col-lg-5 col-md-12 col-sm-12 col-xs-12"
+                        data-scroll-reveal="enter top move 30px over 0.6s after 0.4s">
+                        <h1>About us/<em style="font-family: 'Abhaya Libre', serif;">අපි ගැන</em></h1>Wibhage.lk is a online examination platform.
+						Through this website,students can face to their examinations as usual before.
+						A huge problem that we as students have faced with the onset of the covid 19 epidemic is the practical conduct of examinations.
+						As a solution to this, students have the opportunity to conduct examinations successfully from home as before under the supervision of a teacher online as a live exam through wibhage.lk. 
+						Our platform consists of advanced features to achive this object.
+						
+                    </div>
+					</div>
+					<div class="row">
+					<div class="left-text col-lg-8 col-md-12 col-sm-12 col-xs-12"
+                        data-scroll-reveal="enter down move 30px over 0.6s after 0.4s">
+                        <h1>Designed and developed by<em style="font-family: 'Abhaya Libre', serif;">/මම ගැන</em></h1>
+							<ul style="font-size:30px;">
+							<li><h3>Dinil Hansara<sup>(dip. in Web Engineering-Esoft Metro Campus)</sup></h3></li>							
+							<li><h3 style="font-size:30px;">G/Richmond College</h3></li>
+							<li><h3 style="font-size:30px;">Grade 13</h3></li>
+							<li><h3 style="font-size:30px;"><b>#PROJECT_FUTURE_HOPES</b></h3></li>
+							
+						</ul>
+						
+                    </div>
+                    </div>
+                </div>
+            </div>
+			<?php require("includes/footer.php");?>
+        </div>
+
+	
+    <!-- jQuery 
+    <script src="landing_page/assets/js/jquery-2.1.0.min.js"></script>-->
+
+    <!-- Bootstrap -->
+    <script src="landing_page/assets/js/popper.js"></script>
+    <script src="landing_page/assets/js/bootstrap.min.js"></script>
+
+    <!-- Plugins -->
+    <script src="landing_page/assets/js/owl-carousel.js"></script>
+    <script src="landing_page/assets/js/scrollreveal.min.js"></script>
+    <script src="landing_page/assets/js/waypoints.min.js"></script>
+    <script src="landing_page/assets/js/jquery.counterup.min.js"></script>
+    <script src="landing_page/assets/js/imgfix.min.js"></script>
+
+    <!-- /*Designed and developed by Dinil Hansara as a project for YCS*/ -->
+    <script src="landing_page/assets/js/custom.js"></script>
+
+</body>
+</html>
